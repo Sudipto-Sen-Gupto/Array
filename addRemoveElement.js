aa3b617e4e37c0d4e3533077c5afa9be50444784
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-31 08:27:38 -0500

    add and remove from tourist list

---
+++ addRemoveElement.js
@@ -0,0 +1,14 @@
+// . Add or remove elements
+// .Add a new tourist destination to your tourist array
+// .Declare an array of 3 tourist destinations
+// .Add two more to your array
+// .Remove the last tourist destination you have added
+// .display the final array as output
+
+let touristDestination=["Sundarban","Sajek","Coxesbazar","Rangamati","Sent-martin"];
+console.log(touristDestination);
+// let add=touristDestination.push("Lalbag fort")
+touristDestination.push("Lalbag fort","Ahsan Manjil");
+console.log(touristDestination);
+touristDestination.pop();
+console.log(touristDestination);
